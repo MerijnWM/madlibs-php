2b4c5b16222c36830224bae3df3e62571b759c66
MerijnWM <99040385@mydavinci.nl>
2018-02-02 12:15:51 +0100

    Opdracht madlibs-php is klaar. Merijn Zuurmond

---
+++ onkunde.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>madlibs-php</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<header>Mad libs</header>	
+	<nav>
+		<ul>
+			<li><a href="index.html">Er heerst paniek...</a></li>
+			<li><a href="onkunde.html">Onkunde</a></li>
+		</ul>
+	</nav>
+	<main>	
+		<h2>Onkunde</h2>
+		<p>Er zijn veel mensen die niet kunnen <?php echo $_POST["9"] ?>. Neem nou <?php echo $_POST["10"] ?>. Zelf met de hulp van een <?php echo $_POST["12"] ?> of zelf <?php echo $_POST["11"] ?> kan <?php echo $_POST["10"] ?> niet <?php echo $_POST["9"] ?>. Dat heeft niet te maken met een gebrek aan <?php echo $_POST["13"] ?>, maar met een te veel aan <?php echo $_POST["14"] ?>. Te veel <?php echo $_POST["14"] ?> leidt tot <?php echo $_POST["15"] ?> en dat is niet goed als je wilt <?php echo $_POST["9"] ?>. Helaas voor <?php echo $_POST["10"] ?>.</p>		
+	</main>
+	<footer>Deze website is gemaakt door Merijn</footer>
+</body>
+</html>
